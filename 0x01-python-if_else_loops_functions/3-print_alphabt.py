dcfde06a188ef651c28e5be50beb9c3b172f11f7
Chezlin Drude <chezlindrude23@gmail.com>
2021-09-07 18:53:44 +0200

    3-print_alphabt.py
    Fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-letter = 97
-while letter <= 122:
-    print(chr(letter), end = '')
-    letter += 1
-    if letter == 101:
-        letter += 1
-    if letter == 113:
-        letter += 1
+for letter in range(ord('a'), ord('z')+1):
+    if chr(letter) != "q" and chr(letter) != "e":
+        print("{:s}".format(chr(letter)), end='')
