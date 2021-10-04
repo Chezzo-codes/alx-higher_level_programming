b5a69312c18f4f855a3914703e43db65cef4fbcd
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-04 14:16:34 +0200

    Added Task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Module defines properties of a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Initialize object
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Getter width
+        """
+        return self.__width
+    @width.setter
+    def width(self, val):
+        """
+        Setter width
+        """
+        if isinstance(val, int) is False:
+            raise TypeError("width must be an integer")
+        elif val < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = val
+
+    @property
+    def height(self):
+        """
+        Getter height
+        """
+        return self.__height
+    @height.setter
+    def height(self, val):
+        """
+        Setter height
+        """
+        if isinstance(val, int) is False:
+            raise TypeError("height must be an integer")
+        elif val < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = val
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the rectangle
+        0 if height or width is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
