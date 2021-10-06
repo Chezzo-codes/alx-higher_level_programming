9484d369ec9ced3fea161caac3eafab1209f65ad
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-06 17:17:56 +0200

    fix __str__ func

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -77,11 +77,8 @@ class Rectangle:
         Returns string of rectangle made up of '#'
         """
         if self.__width == 0 or self.__height == 0:
-            return ("")
-        else:
-            width = str(Rectangle.print_symbol) * self.__width
-            rectangle = (width + '\n') * self.__height
-            return rectangle
+            return ""
+        return (('#'*self.__width + "\n")*self.__height)[:-1]
 
     def __repr__(self):
         """
