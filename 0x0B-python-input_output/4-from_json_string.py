b55ceb518e6b6291210e0b54b081dc95653a8309
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-13 17:53:45 +0200

    Add task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+"""
+Contains a function that returns
+an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns python object from json string
+    """
+    json_data = json.loads(my_str)
+    return json_data
