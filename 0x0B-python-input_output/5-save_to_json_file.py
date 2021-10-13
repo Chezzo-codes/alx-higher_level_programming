a63c5e7804459f60a329b5ec249536c6bfcb0e4e
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-13 18:29:01 +0200

    fix docs

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import json
 """
 This file contains a function that
 writes an obj to a text file using
 JSON rep
 """
+import json
 
 
 def save_to_json_file(my_obj, filename):
