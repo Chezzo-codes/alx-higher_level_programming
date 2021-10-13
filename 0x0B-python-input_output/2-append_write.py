c181ab02fcdd5e2cee15ffb556693f76e40969ae
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-13 17:11:57 +0200

    fixed docs

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,8 +5,5 @@ Module with funtion to append to a file
 
 
 def append_write(filename="", text=""):
-    """
-    Returns file with appended text
-    """
     with open(filename, 'a', "utf-8") as f:
         return f.write(str(text))
