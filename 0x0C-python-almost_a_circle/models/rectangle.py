9fb474d4ab5c5a81e4d61c0bef57cb31b1c1278b
Chezlin Drude <chezlindrude23@gmail.com>
2021-10-18 14:46:05 +0200

    Added task 4 files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,8 +37,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -96,3 +95,9 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        """
+        Returns the area of a rectangle instance
+        """
+        return (self.__width * self.__height)
