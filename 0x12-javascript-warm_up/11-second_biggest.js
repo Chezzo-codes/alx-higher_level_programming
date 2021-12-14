59050e2b5a0fa9cc5de83467ce75952f6f2e5a89
Chezlin Drude <chezlindrude23@gmail.com>
2021-12-14 15:42:51 +0200

    fixed script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-const allArgs = process.argv;
-const values = allArgs.slice(2);
-
-if (allArgs.length === 2 || values.length === 1) {
-  console.log('0');
+if (process.argv.length < 4) {
+  console.log(0);
 } else {
-  const sortedArr = values.sort();
-  const revArr = sortedArr.reverse();
-  const secondBig = revArr[1];
-  console.log(secondBig);
+  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
 }
